5b35b5c735ba36c6f3b7165a3067a655696d258b
Kyle <klim385@aucklanduni.ac.nz>
2025-04-05 17:21:57 +1300

    Fixed contact retrieval and contact creation, enable frontend to fetch all contacts

--- exercise/src/routes/contacts-routes.js
+++ exercise/src/routes/contacts-routes.js
@@ -1,16 +1,41 @@
 import { Router } from "express";
-import { createContact, retrieveContact } from "../data/contacts-dao.js";
+import { retrieveContacts, createContact, retrieveContact } from "../data/contacts-dao.js";
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const contact = await retrieveContact("5f90d372-7bdf-4b39-b621-cf6d3cd975f4");
-    res.send(contact);
+    try {
+        const allContacts = await retrieveContacts();
+        console.log("Getting all contacts...");
+        res.json(allContacts);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to retrieve contacts" });
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    // Retrieve contact from ID passed in as URL parameter
+    const id = req.params.id;
+    const contact = await retrieveContact(id);
+    console.log(`Searching for contact ID ${id}...`);
+
+    // Return error if contact not in contacts list
+    if (!contact) {
+        return res.status(404).json({ message: "Contact not found" });
+    }
+
+    // Return contact if found
+    res.status(200).send(contact);
 });
 
 router.post('/', async (req, res) => {
-    const contactWithID = await createContact(req.body);
-    return res.status(201).send(contactWithID);
+    try {
+        const contactWithID = await createContact(req.body);
+        console.log(`Creating new contact ${req.body.name}...`);
+        return res.status(201).send(contactWithID);
+    } catch (error) {
+        return res.status(400).json({ error: error.toString() });
+    }
 })
 
 export default router;
